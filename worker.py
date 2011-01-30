4271c1330e34e86a9c4a1e4d6811920e44e4f685
Nuke <flow.energy@gmail.com>
2011-01-30 22:36:16 +0300

    DB functionality

--- worker.py
+++ worker.py
@@ -2,7 +2,7 @@ from subprocess import Popen, PIPE
 from celery.decorators import task 
 from celery.task import Task
 from celery.task.sets import subtask
-import re, pymongo
+import re, pymongo, bson
 
 QTypes = {"LQ" : "360",
           "SQ" : "480",
@@ -25,8 +25,21 @@ formats = {"4:3" : {
 FFMPEG = "ffmpeg"
 SAVE_PATH = r"~/convert"
 
+class DB(object):
+    def __init__(self):
+        self.connection = Connection('192.168.1.5', 27017)
+        self.database = Database(self.connection, "database")
+        self.collection = database.videos
+    @property
+    def collection(self):
+        return self.collection
+    def __del__(self):
+        self.connection.disconnect()
+        
+
 @task(name="analyze")
-def analyze(name, path, aspect, height, callback = None, **kwargs):
+def analyze(name, path, aspect, height, oid, callback = None, **kwargs):
+    id = bson.ObjectId(oid=oid)
     height = height
     path = path
     name = "%s.%s" % (name, 'flv')
@@ -51,12 +64,14 @@ def analyze(name, path, aspect, height, callback = None, **kwargs):
     log.info("Starting analyze task with params %s [%s]" % (path, quality))
     
     if callback is not None:
-        subtask(callback).delay(name, path, quality)
+        subtask(callback).delay(id, name, path, quality)
 
-@task(name="convert")
-def convert(name, path, quality, callback = None, **kwargs):
+@task(name="convert", max_retries=2)
+def convert(id, name, path, quality, callback = None, **kwargs):
+    db = DB()
     log = Task.get_logger(**kwargs)
-    Task.max_retries = 2
+    finish_pattern = re.compile(r"video:(?P<video>\d+)kB\s*audio:(?P<audio>\d+)kB\s*global\sheaders:(?P<headers>\d+)kB\s*muxing\soverhead\s(\d+\.?\d+)",re.X)
+    
     optdict = { 
                 "FFMPEG" : FFMPEG,
                 "INPUT_FILE" : path,
@@ -66,27 +81,20 @@ def convert(name, path, quality, callback = None, **kwargs):
                 "FILEPATH" : '%s/%s' % (SAVE_PATH,name)
                }
     options = "{FFMPEG} -i {INPUT_FILE} -sn -f {FORMAT} {QUALITY} {ADDITIONAL_OPTS} {FILEPATH}".format(**optdict)
+    
     log.info("Converting process for [%s] starting with params [%s]" % (name, optdict))
-    process = Popen(options, shell=True, stderr=PIPE)
-    finish_pattern = re.compile(r"video:(?P<video>\d+)kB\s*audio:(?P<audio>\d+)kB\s*global\sheaders:(?P<headers>\d+)kB\s*muxing\soverhead\s(\d+\.?\d+)",re.X)
+    
+    process = Popen(options, shell=True, stderr=PIPE)    
     output = process.stderr.read()
     log.debug(output)
     match = finish_pattern.match(output)
     if not match:
-#   	convert.retry([name, path, quality, callback], kwargs, countdown=60)
         log.error("Converting process failed, retrying.")
+        db.collection.update({'_id' : oid }, {{'$set' : {'failed' : 'True'}}, {'$inc' : {'retries' : 1}}})
     else:
+        db.collection.update({'_id' : oid }, {'$set' : {'converted' : 'True'}})
         log.info("Converting process finished successfully.")
-    ### todo functional objects: chunk read output
-    #fcntl.fcntl(process.stderr.fileno(), fcntl.F_SETFL, fcntl.fcntl(process.stderr.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK,)
-    #pattern = re.compile("\S+\s+(?P<frame>\d+)
-    #			\s\S+\s+(?P<fps>\d+)
-    #			\sq=(?P<q>\S+)
-    #			\s\S+\s+(?P<size>\S+)
-    #			\stime=(?P<time>\S+)
-    #			\sbitrate=(?P<bitrate>[\d\.]+)
-    #			""", re.X)"
-    
+
     if callback is not None:
         subtask(callback).delay(path, name)
    
@@ -99,4 +107,15 @@ def thumbnails(path, name, **kwargs):
                 "OUTPUT" : "%s\%s.%s.jpg" % (SAVE_PATH, name, "%d")
                }
     options = "{FFMPEG} -i {INPUT_FILE} -f image2 -r 0.1 -t 5 -vframes 4 -s {SIZE} {OUTPUT}".format(optdict)
-    
+
+def todo():
+    ### todo functional objects: chunk read output
+    #fcntl.fcntl(process.stderr.fileno(), fcntl.F_SETFL, fcntl.fcntl(process.stderr.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK,)
+    #pattern = re.compile("\S+\s+(?P<frame>\d+)
+    #            \s\S+\s+(?P<fps>\d+)
+    #            \sq=(?P<q>\S+)
+    #            \s\S+\s+(?P<size>\S+)
+    #            \stime=(?P<time>\S+)
+    #            \sbitrate=(?P<bitrate>[\d\.]+)
+    #            """, re.X)"
+    #       convert.retry([name, path, quality, callback], kwargs, countdown=60)
